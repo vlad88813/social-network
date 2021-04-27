789132125d771b11ad4335cdc15a981da19a05f6
vlad88813 <82585083+vlad88813@users.noreply.github.com>
2021-04-27 13:25:11 +0400

    social-network

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () =>{
+    return <header className='header'>
+    <img src='https://about.gitlab.com/images/press/logo/png/gitlab-logo-gray-rgb.png' />
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className='content'>
+    <div>
+        <img src='https://moya-planeta.ru/upload/images/xl/12/74/1274272b44a29b045a4466d1cdf2ab79.jpg'/>
+  </div>
+  <div>
+    ava + description
+  </div>
+  <div>
+    my post
+  </div>
+  <div>
+    New post
+  </div>
+  <div>
+     <div>
+       post 1
+     </div>
+  </div>
+  </div>
+};
+
+export default Profile;
