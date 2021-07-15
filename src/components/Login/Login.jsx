c0c820fa9f614b0cc2247fce84b39dd923a1d3d5
vlad88813 <82585083+vlad88813@users.noreply.github.com>
2021-07-15 18:43:28 +0400

    social-network_27
    Yup
    npm install -S yup
    
    +
    
    Formik
    
    сделал форму логинизации.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Formik,Form, Field, ErrorMessage  } from 'formik';
+import * as yup from 'yup';
 
 
 const Login = (props) => {
+
+    let schema = yup.object().shape({
+        login:yup.string().required('Обязательно').matches(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,'Ведите ваш e-mail').min(5, 'Должно быть минимум 5 символов').max(15, 'Максимум 15 символов'),
+        password:yup.string().required('Обязательно').matches(/^[0-9]+$/, "Должно быть числом").min(5, 'Должно быть минимум 5 символов').max(15, 'Максимум 15 символов'),
+        confirmPassword:yup.string().oneOf([yup.ref('password')],'Пароли должны совпадать!').required('Обязательно').matches(/^[0-9]+$/, "Должно быть числом").min(5, 'Должно быть минимум 5 символов').max(15, 'Максимум 15 символов'),
+    })
+
     
     return (
         <Formik
@@ -11,32 +19,67 @@ const Login = (props) => {
         initialValues={{
             login:'',
             password:'',
+            confirmPassword:'',
             rememberMe: false,
-
         }}
+        validateOnBlur
+        validationSchema={schema}
+        // onSubmit={(values)=>{console.log(values)}}
+
         onSubmit={() => {
 
             setTimeout(() => {
-   
               alert('hi-hi');
-   
             }, 1000);
         }}
         >
-            {({}) => (
+            {({values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty }) => (
         <Form onSubmit={props.handleSubmit}>
             {/* onSubmit={props.handleSubmit} ОТПРАВЛЯЕТ ДАННЫЕ С ФОРМЫ В URL */}
+            
+
             <div>
-                <Field placeholder='login' name='login' component='input'/>
+                <label htmlFor={'login'}>User login</label><br/>
+                <Field placeholder='login' name='login' component='input' onChange={handleChange} onBlur={handleBlur} type='string'
+                />
+                {touched.login && errors.login && <p>{errors.login}</p> }
+                {/* old синаксис */}
             </div>
+    
+
+
             <div>
-                <Field  placeholder='Password' name='password' component='input'/>
+                <label htmlFor={'login'}>User Password</label><br/>
+                <Field  placeholder='Password' name='password' component='input'  type='password'
+                />
+                <ErrorMessage name='password' />
             </div>
+            {/* {touched.password && errors.password && <p>{errors.password}</p> } */}
+                {/* onChange={handleChange} onBlur={handleBlur} и др ПРИ ИСПОЛЬЗОВАНИИ FIELD ПРОПИСЫВАТЬ НЕ НУЖНО...boilerplate ЭТО*/} 
+        
+
+            
+
+            <div>
+                <label htmlFor={'login'}>User Password</label><br/>
+                <Field  placeholder='Confirm password' name='confirmPassword' component='input'  type='password'
+                />
+                <ErrorMessage name='confirmPassword' />
+            </div>
+
+
+
             <div>
                 <Field  component='input' name='rememberMe' type='checkbox'/> remember me
             </div>
             <div>
-                <button type='submit' > login </button> 
+            
+
+                <button 
+                type={'submit'}
+                disabled={!isValid && !dirty} 
+                // onClick= {handleSubmit}
+                 > login </button> 
             </div>
         </Form>
             )}
