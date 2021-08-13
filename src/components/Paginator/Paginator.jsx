83bb114440d4d6dc00deaa08aa7a6d70c6ad301c
vlad88813 <82585083+vlad88813@users.noreply.github.com>
2021-08-13 14:13:09 +0400

    social-network_37
    Paginator создал как отдельную компоненту и стили так же.

---
+++ src/components/Paginator/Paginator.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import styles from './Paginator_Style.module.css';
+import cn from 'classnames';
+
+let Paginator = (props) => {
+    let portionSize = 10
+    let pageCount = Math.ceil(props.totalCount / props.pageSize)
+    
+    let pages = [];
+  
+    for (let i=1; i <= Math.ceil(pageCount); i++){
+        pages.push(i);
+    }
+
+
+    let portionCount = Math.ceil(pageCount / portionSize);
+  
+    let [portionNumber,setPortionNumber] = useState(1);
+    useEffect(()=>setPortionNumber(Math.ceil(props.currentPage/portionSize)), [props.currentPage]);  // при уходе со страницы юзеров на другую и при повторном возвращении на неё, пагинация подгоняется под текущую страницу юзеров, которая записана в userReducer
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize; 
+
+    return <div>
+    {portionNumber > 1 &&
+    <button onClick={()=> {setPortionNumber(portionNumber-1) }}> pred </button>}
+    {pages
+    .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+    .map((p) => {
+        return  <span className= {cn ({
+            [styles.selectedPage]: props.currentPage === p
+        }, styles.pageNumber)}
+        key={p}
+        onClick={(e)=>{
+            props.onPageChanged(p);
+        }}>{p}</span> 
+    })}
+    
+{portionCount > portionNumber && 
+<button onClick={()=>{setPortionNumber(portionNumber+1)}}> Next </button>}
+</div>
+}
+
+export default Paginator;
--- src/components/users/UsersRENDER.jsx
+++ src/components/users/UsersRENDER.jsx
@@ -1,54 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Users.module.css';
 import userIMG from '../../assets/img/user_logo.jpg';
 import { NavLink } from 'react-router-dom';
 import YES from '../loader/YES';
-import cn from 'classnames';
+import Paginator from '../Paginator/Paginator'
 
 // чистая компонента 
 
 let UsersRENDER = (props) => {
 
-    let portionSize = 10
-    let pageCount = Math.ceil(props.totalCount / props.pageSize)
+    // let portionSize = 10
+    // let pageCount = Math.ceil(props.totalCount / props.pageSize)
     
-    let pages = [];
+    // let pages = [];
   
-    for (let i=1; i <= Math.ceil(pageCount); i++){
-        pages.push(i);
-    }
-//portionSize
-//Можно добавить ещё:
-// useEffect(()=>setPortionNumber(Math.ceil(currentPage/portionSize)), [currentPage]);
-// при уходе со страницы юзеров на другую и при повторном возвращении на неё, пагинация подгоняется под текущую страницу юзеров, которая записана в userReducer, но лучше самим увидеть разницу.
-//    let portionSize = 5;
+    // for (let i=1; i <= Math.ceil(pageCount); i++){
+    //     pages.push(i);
+    // }
 
 
-    let portionCount = Math.ceil(pageCount / portionSize);
- 
-    let [portionNumber,setPortionNumber] = useState(1);
-    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
-    let rightPortionPageNumber = portionNumber * portionSize; 
+    // let portionCount = Math.ceil(pageCount / portionSize);
+  
+    // let [portionNumber,setPortionNumber] = useState(1);
+    // useEffect(()=>setPortionNumber(Math.ceil(props.currentPage/portionSize)), [props.currentPage]);  // при уходе со страницы юзеров на другую и при повторном возвращении на неё, пагинация подгоняется под текущую страницу юзеров, которая записана в userReducer
+    // let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    // let rightPortionPageNumber = portionNumber * portionSize; 
 
         return <div>
-            <div>
-            {portionNumber > 1 &&
-            <button onClick={()=> {setPortionNumber(portionNumber-1) }}> pred </button>}
-            {pages
-            .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
-            .map((p) => {
-                return  <span className= {cn ({
-                    [styles.selectedPage]: props.currentPage === p
-                }, styles.pageNumber)}
-                key={p}
-                onClick={(e)=>{
-                    props.onPageChanged(p);
-                }}>{p}</span> 
-            })}
-            
-        {portionCount > portionNumber && 
-        <button onClick={()=>{setPortionNumber(portionNumber+1)}}> last </button>}
-        </div>
+        <Paginator 
+        totalCount={props.totalCount}
+        pageSize={props.pageSize}
+        currentPage={props.currentPage}
+        onPageChanged={props.onPageChanged}
+         />
 
         {/* <div>
             {pages.map(p=>{
